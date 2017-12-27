e40ee6f9cf6d7628e38673bd896ba906c4bb353d
Jonathan Maurice <madmaurice@hotmail.com>
2017-12-27 12:11:18 -0500

    3- Octaves

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = target as MapGenerator;
+
+        // DrawDefaultInspector return true if something changed
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+                mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
